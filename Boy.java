3247274b8d8ec8312afed9c59016c2991b76a842
iit2015143 <iit2015143@iiita.ac.in>
2017-02-25 20:00:00 +0530

    Add files via upload

---
+++ Boy.java
@@ -0,0 +1,21 @@
+public class Boy{
+	String name;
+	int attractiveness;
+	int main_budget;
+	int intelligence;
+	int min_att_requir;
+	String status;
+	public Boy(String na, int attr,int main,int in,int min){
+		name = na;
+		attractiveness= attr;
+		main_budget = main;
+		intelligence = in;
+		min_att_requir = min;
+		status = "single";
+	}
+	/*TESTER FUNCTION
+	public static void main(String args[]){
+		Boy Amit =new Boy("Amit",5,1,4,7);
+		System.out.println(Amit.name + "" + Amit.attractiveness+Amit.min_att_requir);
+	}*/
+}
