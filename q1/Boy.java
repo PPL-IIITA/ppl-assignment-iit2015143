7a88895dba948a04326e617385a4057ad265f0cc
iit2015143 <iit2015143@iiita.ac.in>
2017-02-27 01:58:35 +0530

    Update Boy.java

--- q1/Boy.java
+++ q1/Boy.java
@@ -1,5 +1,3 @@
-
-
 public class Boy{
 	String name;
 	int attractiveness;
@@ -15,9 +13,4 @@ public class Boy{
 		min_att_requir = min;
 		status = "single";
 	}
-	/*TESTER FUNCTION
-	public static void main(String args[]){
-		Boy Amit =new Boy("Amit",5,1,4,7);
-		System.out.println(Amit.name + "" + Amit.attractiveness+Amit.min_att_requir);
-	}*/
 }
