50087dec721a57dd3685bd961e8f2d4166f5bd4e
iit2015143 <iit2015143@iiita.ac.in>
2017-04-10 04:39:27 +0530

    Add files via upload

---
+++ q4/doc/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['allocategifts',['Allocategifts',['../classAllocategifts.html#a283c47b5939a5ce1bee7204836331d3b',1,'Allocategifts']]]
+];
---
+++ q4/doc/html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['boy',['Boy',['../classBoy.html#ac94cb08344564d49cfccddce1af0468f',1,'Boy']]],
+  ['breakup',['Breakup',['../classBreakup.html#a554b39edf04e9d5a70bec1881c55cc02',1,'Breakup']]]
+];
