c9f28a1980b7f2646597c3d1a394725169206a0b
iit2015143 <iit2015143@iiita.ac.in>
2017-04-10 04:58:25 +0530

    Add files via upload

---
+++ q5/doc/html/search/functions_5.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['geeks',['Geeks',['../classGeeks.html#ac037a27cb7ceccdbf46d7003c161bd8c',1,'Geeks']]],
+  ['generous',['Generous',['../classGenerous.html#a4db5f95abd7245664c0c58a6b096f12a',1,'Generous']]],
+  ['get',['get',['../classGetname.html#ad09b7c6b03f48f9f61c346d296dac0af',1,'Getname']]],
+  ['gift',['Gift',['../classGift.html#a592557c8fc3205303a4cef7de9faf697',1,'Gift']]],
+  ['giftsort',['Giftsort',['../classGiftsort.html#a609626470d92093872116d489f9d4ea6',1,'Giftsort']]],
+  ['girl',['Girl',['../classGirl.html#ac1044db581d0c0b811dcdc9da386b482',1,'Girl']]]
+];
---
+++ q5/doc/html/search/functions_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['normal',['Normal',['../classNormal.html#a8a4fb738764f30493156d00bbb7b834a',1,'Normal']]]
+];
